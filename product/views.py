9c2a6649a6f9f8c88e7ccf306dd2c1be40021f0e
thisisziihee <iamthejiheee@naver.com>
2020-01-24 12:12:10 +0900

    modeling Product, RegisterProduct

---
+++ product/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from .models import Product
+from .forms import RegisterForm
+from django.views.generic.edit import FormView
+from django.utils.decorators import method_decorator
+from users.decorators import admin_required
+
+
+@method_decorator(admin_required, name = 'dispatch')
+class ProductRegister(FormView):
+    template_name = 'product_register.html'
+    form_class = RegisterForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        product = Product(
+            name = form.data.get('name'),
+            price = form.data.get('price'),
+            stock = form.data.get('stock'),
+            description = form.data.get('description')
+        )
+        product.save()
+        return super().form_valid(form)
