770bd454600e168cb7ab83ba97945e9f9657f773
thisisziihee <iamthejiheee@naver.com>
2020-01-27 15:42:29 +0900

    order List API

--- order/views.py
+++ order/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import FormView
+from django.views.generic import ListView
 from django.utils.decorators import method_decorator
 from django.db import transaction
 
@@ -17,12 +18,13 @@ class OrderCreate(FormView):
 
     def form_valid(self, form):
         with transaction.atomic():
-            prod = Product.objects.gt(pk=form.data.get('product'))
+            prod = Product.objects.get(pk=form.data.get('product'))
             order = Order(
                 quantity = form.data.get('quantity'),
                 product = prod,
                 user = Users.objects.get(email = self.request.session.get('user'))
             )
+            print(order)
             order.save()
 
             prod.stock -= int(form.data.get('quantity'))
@@ -41,4 +43,17 @@ class OrderCreate(FormView):
         })
         return kw
 
+
+@method_decorator(login_required, name = 'dispatch')
+class OrderList(ListView):
+        # model = Order 을 해버리면 문제점이 뭐냐면, 로그인한 사용자에 대한 주문 정보만 나오면 되는데
+        # 모든 사용자에 대한 주문 정보가 나온다. 그래서 session을 통해서 email 값을 받아오는 것이 필요하다.
+        # queryset을 통해서 session 값 가져오는 것
+    #model = Order
+    template_name = "order.html"
+    context_object_name = 'order_list'
+
+    def get_queryset(self, **kwargs):
+        queryset = Order.objects.filter(user__email = self.request.session.get('user'))
+        return queryset
 
